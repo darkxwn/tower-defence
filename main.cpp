4393dd54357075547c3e624e1711990b5ca9cd2b
BillVLad <vladshevchenko29@ya.ru>
2026-03-23 21:58:06 +0300

    Я шатал в рот макос

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include "Game.hpp"
 #include <iostream>
+#ifdef __APPLE__
+#include <CoreFoundation/CoreFoundation.h>
+#include <unistd.h>
+#endif
 
 int main() {
+#ifdef __APPLE__
+    // Этот код находит папку Resources внутри твоего .app
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+    char path[1024];
+    if (CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8*)path, 1024)) {
+        chdir(path); // Переходим в папку Resources
+    }
+    CFRelease(resourcesURL);
+#endif
     setlocale(LC_ALL, "ru");
     try {
         Game game;
@@ -9,7 +23,6 @@ int main() {
     }
     catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
-        system("pause");
     }
 
 	return 0;
